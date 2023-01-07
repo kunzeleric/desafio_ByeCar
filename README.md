d9fe132499ccfdb27711896e2e625e3a1f120ff7
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-01-07 12:50:41 -0300

    Desafio Concluido

---
+++ README.md
@@ -0,0 +1,1 @@
+Atividade Dinamica Desafio - Live 05 - Feito o HTML e CSS da página, e a função de scroll para mudar o card da Moto quando ocorre um click na Seta.
---
+++ main.js
@@ -0,0 +1,27 @@
+
+// definindo uma variavel com estado inicial
+let index = 0;
+let carouselItems = document.querySelectorAll(".card-item") //seleciona os elementos de classe card-item
+
+// função do botão da direita
+function goNext(){
+    if(index === carouselItems.length-1){
+        index = 0; //volta pro primeiro elemento caso chegue no final do array
+    }
+    else{
+        index++;
+    }
+
+    carouselItems[index].scrollIntoView({behavior: "smooth"});
+}
+
+// função do botão da esquerda
+function goBack(){
+    if(index === 0){
+        index = carouselItems.length-1; //caso tente ir para esquerda no primeiro elemento, ele vai pro último card
+    }
+    else{
+        index--;
+    }
+    carouselItems[index].scrollIntoView({behavior: "smooth"});
+}
