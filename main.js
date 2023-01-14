749d343d949d503c4a4213412708ed3c76ab2468
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-01-14 19:37:12 -0300

    Modal adicionado

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+const cardMoto = [
+    {
+      title: "Scooter Elétrica Voltz EV1",
+      paragraph:
+        "O câmbio de 5 marchas da Scooter Elétrica Voltz EV1 oferece maior confiança e performance na pilotagem, tornando as subidas de marcha mais leves e as reduções mais suaves!",
+      img: "/assets/imagem1.png",
+    },
+    {
+      title: "Honda CB 500X",
+      paragraph:
+        "O câmbio de 6 marchas da CB 500X oferece maior confiança e performance na pilotagem, tornando as subidas de marcha mais leves e as reduções mais suaves!",
+      img: "/assets/imagem2.png",
+    },
+  ];
 
 // definindo uma variavel com estado inicial
 let index = 0;
 let carouselItems = document.querySelectorAll(".card-item") //seleciona os elementos de classe card-item
+let modalCard = document.querySelector("#modal")
 
 // função do botão da direita
 goNext = () => {
@@ -25,3 +40,21 @@ goBack = () => {
     }
     carouselItems[index].scrollIntoView({behavior: "smooth"});
 }
+
+showModal = (index) => {
+    modalCard.innerHTML = "";
+    modalCard.innerHTML = `
+    <div class="modal-card">
+    <button  onclick="closeModal()"><img src="assets/close-btn.png" alt="Fechar janela"></button>
+    <h1>${cardMoto[index].title}</h1>
+    <p>${cardMoto[index].paragraph}</p>
+    <img src="${cardMoto[index].img}" alt="Foto da Moto">
+    <button>Quero Assinar!</button>
+    </div>`
+
+    modalCard.style.visibility = "visible";
+}
+
+closeModal = () => {
+    modalCard.style.visibility = "hidden";
+  }
